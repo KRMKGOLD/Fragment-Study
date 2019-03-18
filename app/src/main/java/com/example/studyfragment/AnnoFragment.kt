543184bbcc7830b84224645b961d0999608de2df
MKGOLD <dkqk0124@naver.com>
2019-03-18 20:27:11 +0900

    프래그먼트 제작, 아이콘 추가

---
+++ app/src/main/java/com/example/studyfragment/AnnoFragment.kt
@@ -0,0 +1,13 @@
+package com.example.studyfragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AnnoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_anno, container, false)
+    }
+}
